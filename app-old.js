009afde8412427285b106fd4c940c1660aa390ce
JUAN PABLO PIZARRO <juanpablopizarro97@gmail.com>
2020-07-21 20:23:14 -0500

    Primer commit

---
+++ app-old.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+http.createServer((req,res)=>{
+    res.writeHead(200,{'Content-Type': 'application/json'});
+    
+    let salida ={
+        nombre:'Juan Pablo',
+        edad:'22',
+        url: req.url
+    }
+    res.write(JSON.stringify(salida))
+    //res.write('Hola mundo');
+    res.end();
+})
+.listen(8080);
+
+console.log('Escuchando el puerto 8080');
